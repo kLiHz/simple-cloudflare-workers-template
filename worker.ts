f263bb377b793001ab4fbc98f6de809655adbb19
kLiHz <66966137+kLiHz@users.noreply.github.com>
2024-05-29 19:30:21 +0800

    Fetch and cache Cloudflare Workers' icon for 'favicon.ico'

--- worker.ts
+++ worker.ts
@@ -4,6 +4,29 @@ export interface Env {
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response('Hi!');
+
+    function putCache(cacheKey: Request, response: Response) {
+      ctx.waitUntil(caches.default.put(cacheKey, response.clone()));
+      return response;
+    }
+
+    const url = new URL(request.url);
+
+    if (url.pathname == '/favicon.ico') {
+      const cacheKey = new Request(request.url);
+      const cachedResponse = await caches.default.match(cacheKey);
+      if (!cachedResponse) {
+        const favicon = await fetch('https://workers.cloudflare.com/favicon.ico');
+        return putCache(cacheKey, new Response(favicon.body, {
+          headers: {
+            'Cache-Control': 's-maxage=86400',
+            'Content-Type': favicon.headers.get('Content-Type') || 'image/vnd.microsoft.icon',
+          },
+        }));
+      }
+      return cachedResponse;
+    }
+
+    return new Response(`Hi, you're visiting "${url.pathname}" under "${url.origin}".`);
   },
 };
